8dc878c37c7d95e71763f71f54ae4b5ddaaebdd1
Mahesh Prajapati <mp675089@gmail.com>
2025-09-24 16:27:22 +0530

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     bucket         = "devops-accelerator-platform-tf-state"
     key            = "global/devops-accelerator/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-west-1"
     dynamodb_table = "devops-accelerator-tf-locker"
     encrypt        = true
   }
@@ -367,4 +367,5 @@ resource "aws_iam_policy" "devops_accelerator_lambda_sns_policy" {
 resource "aws_iam_role_policy_attachment" "lambda_sns_policy_attachment" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = aws_iam_policy.devops_accelerator_lambda_sns_policy.arn
+
 }
